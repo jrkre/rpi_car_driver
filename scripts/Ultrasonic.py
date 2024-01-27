7c73ba209993948fae5eea6755d9a834df22d9d5
Joshua Knight <joshuaknight@nevada.unr.edu>
2024-01-27 14:15:28 -0800

    that makes a lot of sense

--- scripts/Ultrasonic.py
+++ scripts/Ultrasonic.py
@@ -2,10 +2,10 @@
 
 from sensor_msgs.msg import Range
 import time
-import Motor
+# import Motor
 import RPi.GPIO as GPIO
 # from servo import *
-import PCA9685
+# import PCA9685
 
 
 class Ultrasonic:
@@ -43,92 +43,92 @@ class Ultrasonic:
         distance_cm = sorted(distance_cm)
         return int(distance_cm[2])
 
-    def run_motor(self, L, M, R):
-        if (L < 30 and M < 30 and R < 30) or M < 30:
-            self.PWM.setMotorModel(-1000, -1000, -1000, -1000)
-            time.sleep(0.1)
-            if L < R:
-                self.PWM.setMotorModel(1000, 1000, -1000, -1000)
-            else:
-                self.PWM.setMotorModel(-1000, -1000, 1000, 1000)
-        elif L < 30 and M < 30:
-            PWM.setMotorModel(1500, 1500, -1500, -1500)
-        elif R < 30 and M < 30:
-            PWM.setMotorModel(-1500, -1500, 1500, 1500)
-        elif L < 20:
-            PWM.setMotorModel(1500, 1500, -500, -500)
-            if L < 10:
-                PWM.setMotorModel(1500, 1500, -1000, -1000)
-        elif R < 20:
-            PWM.setMotorModel(-500, -500, 1500, 1500)
-            if R < 10:
-                PWM.setMotorModel(-1000, -1000, 1000, 1000)
-        else:
-            self.PWM.setMotorModel(600, 600, 600, 600)
+    # def run_motor(self, L, M, R):
+    #     if (L < 30 and M < 30 and R < 30) or M < 30:
+    #         self.PWM.setMotorModel(-1000, -1000, -1000, -1000)
+    #         time.sleep(0.1)
+    #         if L < R:
+    #             self.PWM.setMotorModel(1000, 1000, -1000, -1000)
+    #         else:
+    #             self.PWM.setMotorModel(-1000, -1000, 1000, 1000)
+    #     elif L < 30 and M < 30:
+    #         PWM.setMotorModel(1500, 1500, -1500, -1500)
+    #     elif R < 30 and M < 30:
+    #         PWM.setMotorModel(-1500, -1500, 1500, 1500)
+    #     elif L < 20:
+    #         PWM.setMotorModel(1500, 1500, -500, -500)
+    #         if L < 10:
+    #             PWM.setMotorModel(1500, 1500, -1000, -1000)
+    #     elif R < 20:
+    #         PWM.setMotorModel(-500, -500, 1500, 1500)
+    #         if R < 10:
+    #             PWM.setMotorModel(-1000, -1000, 1000, 1000)
+    #     else:
+    #         self.PWM.setMotorModel(600, 600, 600, 600)
 
-    def run(self):
-        self.PWM = Motor()
-        self.pwm_S = Servo()
+    # def run(self):
+    #     self.PWM = Motor()
+    #     self.pwm_S = Servo()
 
-        while True:
-            self.pwm_S.setServoPwm("0", 90)
-            time.sleep(0.1)
-            M = self.get_distance()
+    #     while True:
+    #         self.pwm_S.setServoPwm("0", 90)
+    #         time.sleep(0.1)
+    #         M = self.get_distance()
 
-            if M < 30:
-                self.pwm_S.setServoPwm("0", 30)
-                time.sleep(0.2)
-                L = self.get_distance()
-                self.pwm_S.setServoPwm("0", 151)
-                time.sleep(0.2)
-                R = self.get_distance()
-                self.run_motor(L, M, R)
-                self.pwm_S.setServoPwm("0", 90)
-            else:
-                self.run_motor(20, M, 20)
+    #         if M < 30:
+    #             self.pwm_S.setServoPwm("0", 30)
+    #             time.sleep(0.2)
+    #             L = self.get_distance()
+    #             self.pwm_S.setServoPwm("0", 151)
+    #             time.sleep(0.2)
+    #             R = self.get_distance()
+    #             self.run_motor(L, M, R)
+    #             self.pwm_S.setServoPwm("0", 90)
+    #         else:
+    #             self.run_motor(20, M, 20)
 
-    def run0(self):
-        self.PWM = Motor()
-        self.pwm_S = Servo()
+    # def run0(self):
+    #     self.PWM = Motor()
+    #     self.pwm_S = Servo()
 
-        for i in range(30, 151, 60):
-            self.pwm_S.setServoPwm('0', i)
-            time.sleep(0.2)
-            if i == 30:
-                L = self.get_distance()
-            elif i == 90:
-                M = self.get_distance()
-            else:
-                R = self.get_distance()
-        while True:
-            for i in range(90, 30, -60):
-                self.pwm_S.setServoPwm('0', i)
-                time.sleep(0.2)
-                if i == 30:
-                    L = self.get_distance()
-                elif i == 90:
-                    M = self.get_distance()
-                else:
-                    R = self.get_distance()
-                self.run_motor(L, M, R)
-            for i in range(30, 151, 60):
-                self.pwm_S.setServoPwm('0', i)
-                time.sleep(0.2)
-                if i == 30:
-                    L = self.get_distance()
-                elif i == 90:
-                    M = self.get_distance()
-                else:
-                    R = self.get_distance()
-                self.run_motor(L, M, R)
+    #     for i in range(30, 151, 60):
+    #         self.pwm_S.setServoPwm('0', i)
+    #         time.sleep(0.2)
+    #         if i == 30:
+    #             L = self.get_distance()
+    #         elif i == 90:
+    #             M = self.get_distance()
+    #         else:
+    #             R = self.get_distance()
+    #     while True:
+    #         for i in range(90, 30, -60):
+    #             self.pwm_S.setServoPwm('0', i)
+    #             time.sleep(0.2)
+    #             if i == 30:
+    #                 L = self.get_distance()
+    #             elif i == 90:
+    #                 M = self.get_distance()
+    #             else:
+    #                 R = self.get_distance()
+    #             self.run_motor(L, M, R)
+    #         for i in range(30, 151, 60):
+    #             self.pwm_S.setServoPwm('0', i)
+    #             time.sleep(0.2)
+    #             if i == 30:
+    #                 L = self.get_distance()
+    #             elif i == 90:
+    #                 M = self.get_distance()
+    #             else:
+    #                 R = self.get_distance()
+    #             self.run_motor(L, M, R)
 
 
-ultrasonic = Ultrasonic()
-# Main program logic follows:
-if __name__ == '__main__':
-    print('Program is starting ... ')
-    try:
-        ultrasonic.run()
-    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-        PWM.setMotorModel(0, 0, 0, 0)
-        ultrasonic.pwm_S.setServoPwm('0', 90)
+# ultrasonic = Ultrasonic()
+# # Main program logic follows:
+# if __name__ == '__main__':
+#     print('Program is starting ... ')
+#     try:
+#         # ultrasonic.run()
+#     except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+#         PWM.setMotorModel(0, 0, 0, 0)
+#         ultrasonic.pwm_S.setServoPwm('0', 90)
