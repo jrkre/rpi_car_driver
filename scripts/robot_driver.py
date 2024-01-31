a06d3e92c8d9b9afa6c9e691c5e0af7b67cd1b3f
Joshua Knight <joshuaknight@nevada.unr.edu>
2024-01-31 13:33:35 -0800

    im struggling with main

--- scripts/robot_driver.py
+++ scripts/robot_driver.py
@@ -70,11 +70,11 @@ def main():
         update_sensors()
         rospy.sleep(0.1)
 
-if "___main__" == __name__:
+if __name__ == '__main__':
     rospy.init_node('robot_driver', anonymous=True)
     rospy.Rate(10)
     
-    print ('Program is starting ... ')
+    rospy.logdebug ('Program is starting ... ')
     
     voltage_pub = rospy.Publisher('/battery_state', BatteryState, queue_size=10)
     ultrasonic_pub = rospy.Publisher('/ultrasonic', Range, queue_size=10)
